f8cf35b2817860e0d63c48a26d0b4992e940da55
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-08-12 13:52:34 +0530

    Time: 3 ms (76.40%) | Memory: 9.5 MB (26.86%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -6,9 +6,8 @@ public:
             int left=0,right=n*m-1;
             while(left<=right){
                 int mid=left+(right-left)/2;
-                int mid_val = matrix[mid / m][mid % m];
-                if(mid_val==target)return true;
-                else if(mid_val<target)left=mid+1;
+                if(matrix[mid / m][mid % m]==target)return true;
+                else if(matrix[mid / m][mid % m]<target)left=mid+1;
                 else right=mid-1;
             }
         return false;
