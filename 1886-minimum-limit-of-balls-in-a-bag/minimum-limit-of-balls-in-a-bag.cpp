4198c519a17ceb3790747803da346beee4aebb1b
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-08-13 15:51:09 +0530

    Time: 117 ms (97.23%) | Memory: 56 MB (18.24%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     int minimumSize(vector<int>& nums, int maxOperations) {
         int r=*max_element(nums.begin(),nums.end());
-        int l=1;
-        int ans=r;
+        int l=1,penalty,op=0;
         while(l<r){
-            int penalty=(l+r)/2;
-            int op=0;
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]>penalty){
-                     if(nums[i]%penalty==0){
-                    op+=nums[i]/penalty-1;
-                }else{
-                    op+=nums[i]/penalty;
-                }
-                }
+            penalty=((r-l)>>1)+l;
+            op=0;
+            for(auto &i: nums){
+                op += ((i-1)/penalty);
             }
-            if(op<=maxOperations){
-                ans=penalty;
-                r=penalty;
-            }
-            else{
-                l=penalty+1;
+            if(op>maxOperations){
+                l = penalty+1;
+            }else{
+                r = penalty;
             }
         }
-        return ans;
+        return l;
     }
 };
