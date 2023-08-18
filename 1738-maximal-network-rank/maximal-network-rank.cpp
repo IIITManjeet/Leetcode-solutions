3a07c95ff5654bef6aaf495ef00c83a77646250c
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-08-19 03:31:06 +0530

    Time: 47 ms (99.71%) | Memory: 28 MB (97.35%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        int maxRank=0;
-       vector<int> degree(n, 0);
-        set<pair<int, int>> road_set;
-        
+              std::vector<int> degrees(n, 0);
         for (auto& road : roads) {
-            degree[road[0]]++;
-            degree[road[1]]++;
-            road_set.insert({road[0], road[1]});
-            road_set.insert({road[1], road[0]});
+            degrees[road[0]]++;
+            degrees[road[1]]++;
         }
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = i+1; j < n; ++j) {
-                int rank = degree[i] + degree[j];
-                if (road_set.find({i, j}) != road_set.end()) {
-                    rank--;
-                }
-                maxRank = max(maxRank, rank);
+        
+        std::unordered_set<int> unique_degrees(degrees.begin(), degrees.end());
+        std::vector<int> sorted_degrees(unique_degrees.begin(), unique_degrees.end());
+        std::sort(sorted_degrees.rbegin(), sorted_degrees.rend());
+        
+        int max_deg = sorted_degrees[0];
+        int second_max_deg = sorted_degrees.size() > 1 ? sorted_degrees[1] : 0;
+        
+        int max_count = std::count(degrees.begin(), degrees.end(), max_deg);
+        int second_max_count = std::count(degrees.begin(), degrees.end(), second_max_deg);
+        
+        if (max_count > 1) {
+            int directly_connected = 0;
+            for (auto& road : roads) {
+                if (degrees[road[0]] == max_deg && degrees[road[1]] == max_deg)
+                    directly_connected++;
             }
+            int possible_connections = max_count * (max_count - 1) / 2;
+            return possible_connections == directly_connected ? 2 * max_deg - 1 : 2 * max_deg;
         }
-         return maxRank;
+        
+        int direct_connections_to_second = 0;
+        for (auto& road : roads) {
+            if ((degrees[road[0]] == max_deg && degrees[road[1]] == second_max_deg) ||
+                (degrees[road[0]] == second_max_deg && degrees[road[1]] == max_deg))
+                direct_connections_to_second++;
+        }
+        
+        return second_max_count == direct_connections_to_second ? max_deg + second_max_deg - 1 : max_deg + second_max_deg;
     }
 };
