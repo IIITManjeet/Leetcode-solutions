d774ecdce23507633d16a8271d929bd2fed14367
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-08-19 03:29:56 +0530

    Time: 99 ms (50.00%) | Memory: 39 MB (33.63%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         int maxRank=0;
-        unordered_map<int,unordered_set<int>>adj;
-        for(auto &road:roads){
-            adj[road[0]].insert(road[1]);
-            adj[road[1]].insert(road[0]);
+       vector<int> degree(n, 0);
+        set<pair<int, int>> road_set;
+        
+        for (auto& road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+            road_set.insert({road[0], road[1]});
+            road_set.insert({road[1], road[0]});
         }
 
-        for(int node1=0;node1<n;++node1){
-           for (int node2 = node1 + 1; node2 < n; ++node2) {
-               int currentRank = adj[node1].size() + adj[node2].size();
-                if (adj[node1].find(node2) != adj[node1].end()) {
-                    --currentRank;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j < n; ++j) {
+                int rank = degree[i] + degree[j];
+                if (road_set.find({i, j}) != road_set.end()) {
+                    rank--;
                 }
-                maxRank = max(maxRank, currentRank);
-           } 
+                maxRank = max(maxRank, rank);
+            }
         }
          return maxRank;
     }
