86ce6d9fdc3ca3b7c72d3c1e90da175b11c84f1f
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-08-28 00:13:36 +0530

    Time: 3 ms (32.97%) | Memory: 6.1 MB (12.49%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+       int ans=0;
+        string s= bitset<32> (start).to_string();
+        string str= bitset<32> (goal).to_string();
+        for(int i=0;i<32;i++){
+            if(s[i]!=str[i]) ans++;
+        }
+        return ans;
+    }
+};
