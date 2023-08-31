fb44bdc6af526131c6d5d81ccffa3edc8747cda7
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-01 02:56:14 +0530

    Time: 29 ms (63.36%) | Memory: 14.6 MB (55.48%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int>maxReach(n+1,0);
-        for(int i=0;i<ranges.size();i++){
-            int start=max(0,i-ranges[i]);
-            int end=min(n,i+ranges[i]);
-            maxReach[start]=max(maxReach[start],end);
-        }
-        int taps=0,curr_end=0,next_end=0;
-        for(int i=0;i<=n;i++){
-            if(i>next_end)return -1;
-            if(i>curr_end){
-                taps++;
-                curr_end=next_end;
+        const int INF = 1e9;
+        vector<int> dp(n + 1, INF);
+        dp[0] = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            int tapStart = max(0, i - ranges[i]);
+            int tapEnd = min(n, i + ranges[i]);
+            for (int j = tapStart; j <= tapEnd; j++) {
+                dp[tapEnd] = min(dp[tapEnd], dp[j] + 1);
             }
-            next_end=max(next_end,maxReach[i]);
         }
-        return taps;
+        if (dp[n] == INF) {
+            return -1;
+        }
+        return dp[n];
+
     }
 };
