afd47667b3bcc1339beee8a9c18cedbb0e9b30bc
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-01 01:55:34 +0530

    Time: 3 ms (65.44%) | Memory: 12 MB (61.69%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,22 +1,25 @@
-//! Using Reverse PreOrder Traversal (Recursive)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void RightView(TreeNode* root, int level, vector<int>&ans)
-    {
-        if(root==NULL)
-            return;
-        
-        if(level == ans.size())
-            ans.push_back(root->val);
-        
-        RightView(root->right, level+1, ans);
-        RightView(root->left, level+1, ans);
+    void fun(TreeNode* node,int level,vector<int>& ans){
+        if(!node)return;
+        if(level==ans.size())ans.push_back(node->val);
+        fun(node->right,level+1,ans);
+        fun(node->left,level+1,ans);
     }
-    
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        RightView(root, 0, ans);
-        
+        vector<int>ans;
+        fun(root,0,ans);
         return ans;
     }
 };
