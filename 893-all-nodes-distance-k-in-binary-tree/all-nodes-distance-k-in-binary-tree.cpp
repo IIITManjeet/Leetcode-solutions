3cd8858b2648ba0a3a18599cdb1d00b82b0261e7
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-04 23:09:33 +0530

    Time: 6 ms (64.70%) | Memory: 13.1 MB (52.26%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void makeParent(TreeNode* node,unordered_map<TreeNode*,TreeNode*>& parentCheck){
+        if(!node)return;
+        queue<TreeNode*>q;
+        q.push(node);
+        while(!q.empty()){
+            TreeNode* temp=q.front();
+            q.pop();
+            if(temp->left){
+                parentCheck[temp->left]=temp;
+                q.push(temp->left);
+            }
+            if(temp->right){
+                parentCheck[temp->right]=temp;
+                q.push(temp->right);
+            }
+        }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*>parentCheck;
+        makeParent(root,parentCheck);
+        vector<int>ans;
+        queue<TreeNode*>q;
+        unordered_map<TreeNode*,bool>vis;
+        q.push(target);
+        vis[target]=true;
+        int level=0;
+        while(!q.empty()){
+            int size=q.size();
+            if(level++==k)break;
+            for(int i=0;i<size;i++){
+            TreeNode* temp=q.front();q.pop();
+            if(temp->left && !vis[temp->left]){
+                q.push(temp->left);
+                vis[temp->left]=true;
+            }
+            if(temp->right && !vis[temp->right]){
+                q.push(temp->right);
+                vis[temp->right]=true;
+            }
+            if(parentCheck[temp] && !vis[parentCheck[temp]]){
+                q.push(parentCheck[temp]);
+                vis[parentCheck[temp]]=true;
+            }
+            }
+        }
+        while(!q.empty()){
+            TreeNode* temp=q.front();q.pop();
+            ans.push_back(temp->val);
+        }
+        return ans;
+    }
+};
