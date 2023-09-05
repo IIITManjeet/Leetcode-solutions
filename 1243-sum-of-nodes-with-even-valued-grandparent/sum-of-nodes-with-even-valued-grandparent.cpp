cef7bef60d403edcfcdaf37394eb3692ec9598b0
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-05 17:20:54 +0530

    Time: 36 ms (76.22%) | Memory: 41.3 MB (59.70%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -11,16 +11,29 @@
  */
 class Solution {
 public:
-int solve(TreeNode* root, int parent, int gParent) {
-        if (!root) {
-            return 0;
-        }
-        return solve(root->left, root->val, parent) 
-                + solve(root->right, root->val, parent)
-                + (gParent % 2 ? 0 : root->val);
-    }
-
     int sumEvenGrandparent(TreeNode* root) {
-        return solve(root,-1,-1);
+        if(root==NULL) 
+		     return 0;
+        
+        int sum=0;
+        
+		//check node with even-valued grandparent add grandchildren value in sum if exist
+		
+        if(root->val%2==0){
+            if(root->left!=NULL && root->left->left!=NULL)
+                sum+=root->left->left->val;
+            if(root->left!=NULL && root->left->right!=NULL)
+                sum+=root->left->right->val;
+            if(root->right!=NULL && root->right->right!=NULL)
+                sum+=root->right->right->val;
+            if(root->right!=NULL && root->right->left!=NULL)
+                sum+=root->right->left->val;
+        }
+		
+		//dfs
+        sum+=sumEvenGrandparent(root->left); 
+        sum+=sumEvenGrandparent(root->right); 
+		
+        return sum;
     }
 };
