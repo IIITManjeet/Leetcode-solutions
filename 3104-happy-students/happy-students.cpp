4f73c548103ac4854bb31046316c095867bf4c8d
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-19 03:22:10 +0530

    Time: 64 ms (99.79%) | Memory: 75 MB (8.95%) - LeetSync

--- 3104-happy-students/happy-students.cpp
+++ 3104-happy-students/happy-students.cpp
@@ -1,16 +1,22 @@
 class Solution {
-
 public:
     int countWays(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    int ways =  nums[0] > 0;
-    for (int i = 0; i < nums.size(); ++i) {
-      int count_with = i + 1;
+        int n=nums.size();
+        vector<int>count(n,0);
+        for(auto &el:nums)count[el]+=1;
+        int psum = 0;
+        int res = 1;
+        if (count[0] == 0) {
+            res++;
+        }
 
-      if (count_with > nums[i] && (i + 1 == nums.size() || count_with < nums[i + 1]))
-        ways++;
+        for (int i = 1; i < n; i++) {
+            psum += count[i - 1];
+            if (psum == i && count[i] == 0) {
+                res++;
+            }
+        }
+
+        return res;
     }
-
-    return ways;
-}
 };
