21012e9374893bbedc8c8b59a3b164ee3342bbb9
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-24 12:36:02 +0530

    Time: 10 ms (44.77%) | Memory: 16.6 MB (32.79%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>>a(query_row + 1,vector<double>(query_row + 1,0.0));
-        a[0][0]=static_cast<double>(poured);
-        for(int r=0;r<query_row;r++){
-            for(int c=0;c<=r;c++){
-                double q=(a[r][c]-1.0)/2.0;
-                if(q>0){
-                    a[r+1][c]+=q;
-                    a[r+1][c+1]+=q;
-                }
+vector<double> tower(102, 0.0);
+        tower[0] = poured;
+        for (int i = 0; i < query_row; i++) {
+            vector<double> ntower(102, 0.0);
+            for (int j = 0; j <= i; j++) {
+                double overflow = max(0.0, (tower[j] - 1.0) / 2.0);
+                ntower[j] += overflow;
+                ntower[j + 1] += overflow;
+            }
+            
+            for (int j = 0; j <= i + 1; j++) {
+                tower[j] = ntower[j];
             }
         }
-        return min(1.0,a[query_row][query_glass]);
+        
+        return min(1.0, tower[query_glass]);        
+
     }
 };
