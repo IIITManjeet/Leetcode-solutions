bd25067b949fd480769ae4d9d0f3384ac1956d9a
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-09-24 12:44:21 +0530

    Time: 3 ms (86.54%) | Memory: 12.5 MB (15.16%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> sortEvenOdd(vector<int>& nums) {
-         vector<int> odd, even;
-        for(int i = 0; i < nums.size(); i++) {
-            if(i & 1) {
-                odd.push_back(nums[i]);
-            } else {
-                even.push_back(nums[i]);
+        priority_queue<int, vector<int>, greater<int>> even;
+        priority_queue<int> odd;
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0) even.push(nums[i]);
+            else odd.push(nums[i]);
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0){
+                nums[i] = even.top();
+                even.pop();
+            }else{
+                nums[i] = odd.top();
+                odd.pop();
             }
         }
-        sort(odd.begin(), odd.end(), greater<int>());
-        sort(even.begin(), even.end());
-        for(int i = 0; i < nums.size(); i++) {
-            if(i & 1) {
-                nums[i] = odd[0];
-                odd.erase(odd.begin());
-            } else {
-                nums[i] = even[0];
-                even.erase(even.begin());
-            }
-        }
+        
         return nums;
     }
 };
