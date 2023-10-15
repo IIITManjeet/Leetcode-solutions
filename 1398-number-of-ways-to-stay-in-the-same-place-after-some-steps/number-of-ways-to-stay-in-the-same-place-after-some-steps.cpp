08b6dd8a2ad9cd02ff347aca9a44c1bb1a65df99
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-10-16 00:08:38 +0530

    Time: 19 ms (59.31%) | Memory: 13 MB (54.33%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    const int mod=1e9 + 7;
-    // int arrLen;
-    // int solve(int cur,int remain,vector<vector<int>>& memo){
-    //     if(remain<=0){
-    //         if(cur==0)return 1;
-    //         return 0;
-    //     }
-    //     if(memo[cur][remain]!=-1)return memo[cur][remain];
-    //     int stay=solve(cur,remain-1,memo);
-    //     if (cur> 0) {
-    //         stay = (stay + solve(cur - 1, remain - 1,memo)) % mod;
-    //     }
-        
-    //     if (cur < arrLen - 1) {
-    //         stay = (stay + solve(cur + 1, remain - 1,memo)) % mod;
-    //     }
-        
-    //     memo[cur][remain] = stay;
-    //     return stay;
-    // }
     int numWays(int steps, int arrLen) {
-        arrLen=min(steps,arrLen);
-        vector<vector<int>>memo(arrLen,vector<int>(steps+1,0));
-        memo[0][0] = 1;
-        for(int remain=1;remain<=steps;remain++){
-            for(int cur=arrLen-1;cur>=0;cur--){
-                int stay=memo[cur][remain-1];
-                if(cur>0)stay=(stay+memo[cur-1][remain-1])%mod;
-                if(cur<arrLen-1)stay=(stay+memo[cur+1][remain-1])%mod;
-                memo[cur][remain]=stay;
+        int MOD = 1e9 + 7;
+        arrLen = min(arrLen, steps);
+        vector<int> dp(arrLen, 0);
+        vector<int> prevDp(arrLen, 0);
+        prevDp[0] = 1;
+        
+        for (int remain = 1; remain <= steps; remain++) {
+            dp = vector(arrLen, 0);
+            
+            for (int curr = arrLen - 1; curr >= 0; curr--) {
+                int ans = prevDp[curr];
+                
+                if (curr > 0) {
+                    ans = (ans + prevDp[curr - 1]) % MOD;
+                }
+                
+                if (curr < arrLen - 1) {
+                    ans = (ans + prevDp[curr + 1]) % MOD;
+                }
+                
+                dp[curr] = ans;
             }
+            
+            prevDp = dp;
         }
-        return memo[0][steps];
+        
+        return dp[0];
     }
 };
