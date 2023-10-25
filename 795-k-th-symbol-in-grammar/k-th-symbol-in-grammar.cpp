e26ae56a10673531185615e0afc28e664e4c2745
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-10-25 13:47:29 +0530

    Time: 2 ms (51.37%) | Memory: 6.3 MB (28.86%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+         if (n == 1) {
+            return 0;
+        }
+        int totalElements = pow(2, (n - 1));
+        int halfElements = totalElements / 2;
+        if (k > halfElements) {
+            return 1 - kthGrammar(n, k - halfElements);
+        }
+        return kthGrammar(n - 1, k);
+    }
+};
