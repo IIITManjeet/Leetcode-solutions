5e15861ede7f49cccf16d2e3ec55474c0edd26dc
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-11-13 23:55:44 +0530

    Time: 71 ms (14.41%) | Memory: 13.7 MB (42.60%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i' 
-            || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
-    }
     string sortVowels(string s) {
-        string temp;
-        for(auto c:s){
-            if(isVowel(c))temp+=c;
-        }
-        sort(temp.begin(),temp.end());
-        int j=0;
-        string ans;
-        for(int i=0;i<s.size();i++){
-            if(isVowel(s[i])){
-                ans+=temp[j];
-                j++;
-            }
-            else{
-                ans+=s[i];
+        // Step 1: Collect vowels and sort them in descending order
+        vector<char> vowels;
+        for (char c : s) {
+            if (string("aeiouAEIOU").find(c) != string::npos) {
+                vowels.push_back(c);
             }
         }
-        return ans;
+        sort(vowels.begin(), vowels.end(), greater<char>());
+
+        // Step 2: Construct the answer string by replacing vowels in sorted order
+        string result;
+        for (char c : s) {
+            if (string("aeiouAEIOU").find(c) != string::npos) {
+                result += vowels.back();
+                vowels.pop_back();
+            } else {
+                result += c;
+            }
+        }
+
+        // Step 3: Return the final string
+        return result;        
     }
 };
