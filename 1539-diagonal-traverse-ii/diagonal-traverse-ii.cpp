2031efd5b8b5984ec52cd24e5dd5e58e1c0579da
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-11-24 05:14:52 +0530

    Time: 209 ms (48.87%) | Memory: 97.6 MB (58.97%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        int m = nums.size(), maxSum = 0, size = 0, index = 0;
-        std::vector<std::vector<int>> map(100001);
-        
-        for (int i = 0; i < m; i++) {
-            size += nums[i].size();
-            for (int j = 0; j < nums[i].size(); j++) {
-                int sum = i + j;
-                map[sum].push_back(nums[i][j]);
-                maxSum = std::max(maxSum, sum);
+        unordered_map<int,vector<int>>m;
+        int maxSum=0;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums[i].size();j++){
+                m[i+j].push_back(nums[i][j]);
+                maxSum=max(i+j,maxSum);
             }
         }
-        
-        std::vector<int> res(size);
-        for (int i = 0; i <= maxSum; i++) {
-            std::vector<int>& cur = map[i];
-            for (int j = cur.size() - 1; j >= 0; j--) {
-                res[index++] = cur[j];
+        for(int i=0;i<=maxSum;i++){
+            for(auto x=m[i].rbegin();x!=m[i].rend();x++){
+                ans.push_back(*x);
             }
         }
-        
-        return res;
+        return ans;
     }
 };
