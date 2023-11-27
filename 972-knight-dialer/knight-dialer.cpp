51bbc8ca72e18eab4a5e8a11b6581654611805d5
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2023-11-27 11:59:31 +0530

    Time: 44 ms (85.96%) | Memory: 8.6 MB (69.59%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    int n;
-    int MOD = 1e9 + 7;
-    vector<vector<int>> jumps = {
-        {4, 6},
-        {6, 8},
-        {7, 9},
-        {4, 8},
-        {3, 9, 0},
-        {},
-        {1, 7, 0},
-        {2, 6},
-        {1, 3},
-        {2, 4}
-    };
-    int dp(int remain,int sq){
-        if(remain==0)return 1;
-        if(memo[remain][sq]!=0)return memo[remain][sq];
-        int ans=0;
-        for(auto nextsq:jumps[sq]){
-            ans=(ans+dp(remain-1,nextsq))%MOD;
-        }
-        memo[remain][sq]=ans;
-        return ans;
-    }
+    static const int mod = 1e9 + 7;
+    vector<vector<int>> MOVES = {
+    {4, 6},
+    {8, 6},
+    {7, 9},
+    {4, 8},
+    {3, 9, 0},
+    {},
+    {0, 1, 7},
+    {2, 6},
+    {1, 3},
+    {2, 4},
+};
+
+    int cache[5001][10];
+
     int knightDialer(int n) {
-        this->n=n;
-        memo=vector(n+1,vector<int>(10,0));
-        int ans=0;
-        for(int sq=0;sq<10;sq++)
-        ans=(ans+dp(n-1,sq))%MOD;
-        return ans;
+        vector<int> nextNumbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        return knightDialer(n, nextNumbers);
+    }
+
+    int knightDialer(int remaining, vector<int>& nextNumbers) {
+        if (remaining == 1)
+            return nextNumbers.size();
+
+        int count = 0;
+        for (int nextNumber : nextNumbers) {
+            int cur = cache[remaining][nextNumber];
+            if (cur == 0) {
+                cur = knightDialer(remaining - 1, MOVES[nextNumber]);
+                cache[remaining][nextNumber] = cur;
+            }
+            count += cur;
+            count %= mod;
+        }
+        return count;
     }
 };
