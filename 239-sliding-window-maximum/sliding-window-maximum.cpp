1d73bc4bd534f320821ed4d416953de210513eb6
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-01-03 02:36:02 +0530

    Time: 1264 ms (5.01%) | Memory: 212.4 MB (6.46%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        std::deque<int>dq;
-        std::vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            while(!dq.empty() && dq.front()==i-k)dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<=nums[i])dq.pop_back();
-            dq.push_back(i);
-            if(i>=k-1)ans.push_back(nums[dq.front()]);
-        }
-        return ans;
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    int n = nums.size();
+
+    vector<int> result;
+    set<pair<int, int>, greater<pair<int,int>>> s;
+
+    for(int i = 0; i < k; i++){
+        s.insert(make_pair(nums[i], i));
     }
+
+    auto i = s.begin();
+    result.push_back(i->first);
+
+    for(int i = 0, j = k; j < n; i++, j++){
+        s.erase(make_pair(nums[i], i));
+        s.insert(make_pair(nums[j], j));
+        result.push_back(s.begin()->first);
+    }
+
+    return result;
+}
 };
