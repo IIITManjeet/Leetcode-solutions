eac7cfa5961c69e754ae35dbed4f654c8bbde7f4
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-01-03 02:36:49 +0530

    Time: 1282 ms (5.01%) | Memory: 212.4 MB (6.46%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    int n = nums.size();
-
-    vector<int> result;
-    set<pair<int, int>, greater<pair<int,int>>> s;
-
-    for(int i = 0; i < k; i++){
-        s.insert(make_pair(nums[i], i));
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        multiset<int> st;
+        int i = 0;
+        while ( i < k ) st.insert(nums[i++]);
+        ans.push_back(*st.rbegin());
+        int n = nums.size();
+        for ( int i = k ; i < n ; i++ ) {
+            auto ind = st.find(nums[i-k]);
+            st.erase(ind); st.insert(nums[i]);
+            ans.push_back(*st.rbegin());
+        }
+        return ans;
     }
-
-    auto i = s.begin();
-    result.push_back(i->first);
-
-    for(int i = 0, j = k; j < n; i++, j++){
-        s.erase(make_pair(nums[i], i));
-        s.insert(make_pair(nums[j], j));
-        result.push_back(s.begin()->first);
-    }
-
-    return result;
-}
 };
