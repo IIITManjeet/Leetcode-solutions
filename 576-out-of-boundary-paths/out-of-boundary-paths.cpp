37ae48980b90368272756dabd5dbd7b085dd2871
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-01-27 00:00:25 +0530

    Time: 6 ms (68.27%) | Memory: 7.7 MB (59.44%) - LeetSync

---
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int dp[50][50][51];
+    vector<pair<int,int>> dir={{-1,0},{0,1},{1,0},{0,-1}};
+    int mod=1e9+7;
+public:
+    int f(int stR,int stC,int mm,int n,int m){
+        if(stR>=n || stR<0 || stC>=m || stC<0)return 1;
+        if(mm==0)return 0;
+        int ans=0;
+        if(dp[stR][stC][mm]!=-1)return dp[stR][stC][mm];
+        for(int i=0;i<4;i++){
+            int newR=stR+dir[i].first;
+            int newC=stC+dir[i].second;
+            ans=(ans+f(newR,newC,mm-1,n,m))%mod;
+        }
+        return dp[stR][stC][mm]=ans;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp,-1,sizeof(dp));
+        return f(startRow,startColumn,maxMove,m,n);
+    }
+};
