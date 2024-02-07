9aef2687b89b27718ac772407b589f9fff4986c3
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-02-07 18:46:29 +0530

    Time: 3 ms (99.77%) | Memory: 9.7 MB (38.15%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     string frequencySort(string s) {
-         auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
+        vector<int> freq(256, 0); // Assuming ASCII characters
+        
+        for (char ch : s) {
+            freq[ch]++;
+        }
+        
+        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
             return a.second < b.second;
         };
-        
         priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
-        map<char,int>mp;
-        for(auto ch:s){
-            mp[ch]++;
-        }
-        for(auto it:mp){
-            pq.push({it.first,it.second});
-        }
-        string ans="";
-        while(!pq.empty()){
-            char ch=pq.top().first;
-            int cnt=pq.top().second;
-            for(int i=0;i<cnt;i++){
-                ans+=ch;
+        
+        for (int i = 0; i < 256; ++i) { // Assuming ASCII characters
+            if (freq[i] > 0) {
+                pq.push({static_cast<char>(i), freq[i]});
             }
+        }
+        
+        string ans = "";
+        while (!pq.empty()) {
+            char ch = pq.top().first;
+            int cnt = pq.top().second;
+            ans.append(cnt, ch);
             pq.pop();
         }
+        
         return ans;
     }
 };
