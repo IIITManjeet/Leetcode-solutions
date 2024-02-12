91d50157abd56682a0e0e64c5d831a1c1c0eb6e7
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-02-12 20:33:32 +0530

    Time: 7 ms (97.54%) | Memory: 21.9 MB (33.81%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority = 0;
-        for (unsigned int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
-            int bits = 0;
-            for (int num : nums) {
-                if (num & mask) {
-                    bits++;
-                }
+        int maj=nums[0],cnt=1;
+        for(int i=1;i<nums.size();i++){
+            int it=nums[i];
+            if(cnt==0){
+                maj=it;
+                cnt++;
+            }else if(maj==it){
+                cnt++;
             }
-            if (bits > nums.size() / 2) {
-                majority |= mask;
-            }
+            else cnt--;
         }
-        return majority;
+        return maj;
     }
 };
