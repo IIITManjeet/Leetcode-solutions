c22e71ce11457ba207dcd737f1f4809669e9e3a0
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-02-22 19:26:18 +0530

    Time: 22 ms (82.11%) | Memory: 22.5 MB (50.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,14 +5,10 @@ public:
         int n=inter.size();
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            int start=inter[i][0],end=inter[i][1];
-            if (!ans.empty() && end <= ans.back()[1])continue;
-            for(int j=i+1;j<n;j++){
-                if(end>=inter[j][0]){
-                    end=max(end,inter[j][1]);
-                }else break;
-            }
-            ans.push_back({start,end});
+            if (ans.empty() || inter[i][0] > ans.back()[1])
+            ans.push_back(inter[i]);
+            else
+            ans.back()[1] = max(ans.back()[1], inter[i][1]);
         }
         return ans;
     }
