6228d43cbce00327a64e2be17f80cd57166e4175
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-03-04 00:46:09 +0530

    Time: 93 ms (41.05%) | Memory: 50.4 MB (45.30%) - LeetSync

--- 881-loud-and-rich/loud-and-rich.cpp
+++ 881-loud-and-rich/loud-and-rich.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    // Modified DFS function with memoization
-    int dfs(int ind, map<int,vector<int>>& gr, vector<int>& sol, vector<int>& quiet){
-        // If the solution for this person is already calculated, return it
-        if(sol[ind] != -1) return sol[ind];
-        
-        int minPos = ind, minQ = quiet[ind];
-        for(int neigh : gr[ind]){
-            int in = dfs(neigh, gr, sol, quiet);
-            if(quiet[in] < minQ){
-                minPos = in;
-                minQ = quiet[in];
+    vector<int> loudAndRich(vector<vector<int>>& r, vector<int>& q) {
+        int n = q.size();
+        vector<int> adj[n];
+        vector<int> ind(n,0);
+        for(auto it:r)
+        {
+            adj[it[0]].push_back(it[1]);
+            ind[it[1]]++;
+        }
+        queue<int> qu;
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+        {
+            if(ind[i] == 0)
+            qu.push(i);
+            ans[i]=i;
+        }
+        while(!qu.empty())
+        {
+            int node = qu.front();
+            qu.pop();
+            for(auto it:adj[node])
+            {
+                if(q[node]<q[it])
+                {
+                    q[it]=q[node];
+                    ans[it]=ans[node];
+                }
+                ind[it]--;
+                if(ind[it] == 0)
+                qu.push(it);
             }
         }
-        // Store the result in the solution array and return it
-        sol[ind] = minPos;
-        return minPos;
-    }
-    
-    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
-        int n = quiet.size();
-        map<int,vector<int>> gr;
-        for(auto it : richer){
-            gr[it[1]].push_back(it[0]); // Reversed the direction of edges
-        }
-        
-        vector<int> sol(n, -1);
-        for(int i = 0; i < n; i++){
-            dfs(i, gr, sol, quiet);
-        }
-        return sol;
+        return ans;
     }
 };
