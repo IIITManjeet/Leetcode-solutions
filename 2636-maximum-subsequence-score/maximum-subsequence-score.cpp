9ef3085c733f86e63a3c615f1937fe11323699ca
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-05-12 03:47:47 +0530

    Time: 154 ms (78.61%) | Memory: 95.6 MB (41.03%) - LeetSync

---
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
@@ -0,0 +1,30 @@
+#define ll long long
+#define PB push_back
+#define all(v) v.begin(), v.end()
+
+class Solution {
+public:
+    ll maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        ll ans = 0LL, sum = 0LL;
+        int n = nums1.size();
+        vector<pair<int,int>> v;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i = 0; i < n; i++){
+            v.PB({nums2[i], nums1[i]});
+        }
+        sort(v.begin(), v.end(), greater<pair<int, int>>());
+
+        for(int j = 0; j < n; j++){
+            sum += v[j].second;
+            pq.push(v[j].second);
+
+            if(pq.size() ==k){
+                ans = max(ans, sum * v[j].first);
+                sum -= pq.top();
+                pq.pop();
+            }
+        }
+
+        return ans;
+    }
+};
