3e993b8d31096eb5e2f67724f00b6920452495ab
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-06-21 07:55:31 +0530

    Time: 2 ms (49.40%) | Memory: 7 MB (86.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+        if(n==1 || n==2)return n;
+        int prev=1,cur=1;
+        for(int i=2;i<=n;i++){
+            int temp=cur;
+            cur=temp+prev;
+            prev=temp;
+        }return cur;
     }
 };
