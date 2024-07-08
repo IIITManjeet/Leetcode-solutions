beae92444b07677d5b997d200304b65047a77d2c
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-07-08 23:20:38 +0530

    Time: 2 ms (75.45%) | Memory: 7.2 MB (62.20%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,21 +1,10 @@
-#include <set>
-
 class Solution {
 public:
-    int findTheWinner(int n, int k) {
-        std::set<int> st;
-        for (int i = 1; i <= n; ++i) st.insert(i);
-        auto it = st.begin();
-        while (st.size() > 1) {
-            for (int count = 1; count < k; ++count) {
-                ++it;
-                if (it == st.end()) it = st.begin();
-            }
-            auto toRemove = it++;
-            if (it == st.end()) it = st.begin();
-            st.erase(toRemove);
-        }
-        
-        return *st.begin();
+    int findTheWinner(int n, int k) { return winnerHelper(n, k) + 1; }
+
+private:
+    int winnerHelper(int n, int k) {
+        if (n == 1) return 0;
+        return (winnerHelper(n - 1, k) + k) % n;
     }
 };
