e4d08a7e55e51f3cd54f8133abb63756e1de03b0
Manjeet Pathak <96388375+IIITManjeet@users.noreply.github.com>
2024-08-20 02:08:02 +0530

    Time: 3 ms (69.44%) | Memory: 7.4 MB (77.36%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        if (n == 1) return 0;
+        
+        int ans = 0;
+        int f = 2;
+        
+        while (n > 1) {
+            while (n % f == 0) {
+                ans += f;
+                n /= f;
+            }
+            f++;
+        }
+        
+        return ans;
+    }
+};
